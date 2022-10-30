32298c459151c56153a04e9def45da0e378eb523
phillip burns <phillipworld@hotmail.co.uk>
2022-10-30 12:15:56 +0000

    Added type to query and database setup

---
+++ models/bookModel.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  unique_url: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  img: String,
+  publisher: String,
+  category: String,
+  topic: String,
+  year: Number,
+  pages: Number,
+  language: String,
+});
+
+const Book = mongoose.models.Book || mongoose.model("Book", bookSchema);
+
+export default Book;
