eac0547722e3d8bf657aca32360371bd05c8695c
phillip burns <phillipworld@hotmail.co.uk>
2022-11-01 10:56:43 +0000

    Small boxshadow change

--- components/bookGrid.tsx
+++ components/bookGrid.tsx
@@ -2,9 +2,5 @@ import GridChildProps from '@/types/gridChildProp';
 import style from '@/styles/bookGrid.module.scss';
 
 export default function BookGrid({ children }: GridChildProps) {
-  return (
-    <div className={`row m-0 col-12`}>
-      {children}
-    </div>
-  );
+  return <div className={`row justify-content-center m-0 col-12`}>{children}</div>;
 }
