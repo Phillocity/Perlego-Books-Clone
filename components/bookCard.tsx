a60ad0fae6961558c14ed3345a9977c633ce733f
phillip burns <phillipworld@hotmail.co.uk>
2022-11-01 00:56:16 +0000

    Alternative card

--- components/bookCard.tsx
+++ components/bookCard.tsx
@@ -6,16 +6,19 @@ import style from '@/styles/bookCard.module.scss';
 export default function Book({ _id, unique_url, title, author, description, img, publisher, category, topic, year, pages }: BookType) {
   return (
     <>
-      <div className={`${style.bookCard} flex-grow-1 px-0 fade-in col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2`} key={_id}>
-        <Link href={unique_url}>
-          <Image src={img} alt={title} width={160} height={245} />
-          <div className={style.cardBorder}></div>
-          <div className={`${style.bookBody} mt-3 mx-2`}>
-            <p className="h6">{title.length > 29 ? title.slice(0, 29) + '...' : title}</p>
-            <p className="h6">{category}</p>
-            <p className="h6 text-muted">{author.split(',')[0]}</p>
-          </div>
-        </Link>
+      <div className={`${style.bookCard} px-0 fade-in col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2`} key={_id}>
+        <div className={`${style.bookItem} pb-3`}>
+          <Link href={unique_url}>
+            <div className={style.bookImage}>
+              <Image src={img} alt={title} width={160} height={245} />
+            </div>
+            <div className={`${style.bookBody} mt-3 mx-2`}>
+              <p className="h6">{title.length > 25 ? title.slice(0, 25) + '...' : title}</p>
+              <p className={`${style.categoryHighlight} h6 text-uppercase`}>{category.split(' ')[0]}</p>
+              <p className="h6 text-muted">By {author.split(',')[0]}</p>
+            </div>
+          </Link>
+        </div>
       </div>
     </>
   );
